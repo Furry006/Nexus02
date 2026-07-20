545ca5fbdafa8c83dec37d444dc39c727d0c6084
Mukul  Singh <ms0897493@gmail.com>
2026-07-20 18:36:04 +0530

    feat: add user table and API response/error handling utilities

--- apps/server/drizzle.config.ts
+++ apps/server/drizzle.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './src/db/schemaDb.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URI!,
+    url: process.env.DATABASE_URL!,
   },
 });
