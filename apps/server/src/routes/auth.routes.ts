9e2e0cff18721c8adf56bdec1c7ba4f3573b96dd
Mukul  Singh <ms0897493@gmail.com>
2026-07-22 18:03:00 +0530

    feat(auth): add login schema and update auth routes for improved functionality

--- apps/server/src/routes/auth.routes.ts
+++ apps/server/src/routes/auth.routes.ts
@@ -1,17 +1,18 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 
-import { register } from "../controllers/auth.controller";
-import { registerSchema } from "../schemas/auth.schema";
+import { register, login, logout, getMe } from "../controllers/auth.controller";
+import { registerSchema, loginSchema } from "../schemas/auth.schema";
+
 
 const authRouter = new Hono();
 
 authRouter.post("/register", zValidator("json", registerSchema), register);
 
-// authRouter.post("/login", zValidator("json", loginSchema), login);
+authRouter.post("/login", zValidator("json", loginSchema), login);
 
-// authRouter.post("/logout", logout);
+authRouter.post("/logout", logout);
 
-// authRouter.get("/me", getMe);
+authRouter.get("/me", getMe);
 
 export default authRouter;
--- apps/server/src/schemas/auth.schema.ts
+++ apps/server/src/schemas/auth.schema.ts
@@ -6,3 +6,13 @@ export const registerSchema = z.object({
   email: z.email("Invalid email"),
   password: z.string().min(8, "Password must be at least 8 characters"),
 });
+
+export const loginSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
