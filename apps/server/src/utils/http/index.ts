a3fd326af61a5121479e2091f58e75c6e9bcd789
Mukul  Singh <ms0897493@gmail.com>
2026-07-23 17:50:06 +0530

    feat(http): add HTTP status codes and phrases from external source

--- apps/server/src/utils/http/index.ts
+++ apps/server/src/utils/http/index.ts
@@ -1,3 +1,4 @@
-export { HttpError } from './error.js'
-export *as HttpResponse from './response.js'
-
+export { HttpError } from "./error.js";
+export * as HttpPhrases from "./phrases.js";
+export * as HttpResponse from "./response.js";
+export * as HttpStatus from "./status.js";
