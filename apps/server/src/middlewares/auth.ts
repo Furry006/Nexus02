7973cc04dc230be03334047ff193ef26230faf6e
Mukul  Singh <ms0897493@gmail.com>
2026-07-27 19:25:13 +0530

    feat(auth): add authentication middleware to verify access tokens

---
+++ apps/server/src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { createMiddleware } from "hono/factory";
+import { getCookie } from "hono/cookie";
+import { HTTPException } from "hono/http-exception";
+
+import { verifyAccessToken } from "#/utils/helpers.js";
+import { HttpStatus } from "#/utils/http/index.js";
+
+export const authMiddleware = createMiddleware(async (c, next) => {
+  const token = getCookie(c, "access_token");
+
+  if (!token) {
+    throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const payload = await verifyAccessToken(token);
+
+    c.set("user", {
+      id: payload["id"] as string,
+      email: payload["email"] as string,
+    });
+
+    await next();
+  } catch {
+    throw new HTTPException(HttpStatus.UNAUTHORIZED, {
+      message: "Invalid or expired access token",
+    });
+  }
+});
