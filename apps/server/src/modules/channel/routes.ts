c5eabc30605baae39f0d1927372a2ec2a90d4eb2
Mukul  Singh <ms0897493@gmail.com>
2026-08-23 20:35:01 +0530

    feat: implement channel creation and retrieval service, routes, and schemas

--- apps/server/src/modules/channel/routes.ts
+++ apps/server/src/modules/channel/routes.ts
@@ -9,7 +9,7 @@ import {
 import { HttpPhrases, HttpStatus } from "#/utils/http/index.js";
 import { authMiddleware } from "#/middlewares/auth.js";
 import { createChannelSchema, workspaceIdParamSchema } from "./schemas.js";
-import { createChannelHandler } from "./handlers.js";
+import { createChannelHandler, getWorkspaceChannelHandler } from "./handlers.js";
 
 
 export const createChannelRoute = createRoute({
@@ -61,8 +61,52 @@ export const createChannelRoute = createRoute({
     },
 });
 
+export const getWorkspaceChannelRoute = createRoute({
+    tags: Tags.Channel,
+    method: "get",
+    path: "/workspace/{workspaceId}/channel",
+    middleware: [authMiddleware],
+    request: {
+        params: workspaceIdParamSchema,
+    },
+    responses: {
+        [HttpStatus.OK]: jsonContent(
+            successSchema({
+                message: "Channel Fetched Successfully",
+            }),
+            HttpPhrases.OK,
+        ),
+        [HttpStatus.BAD_REQUEST]: jsonContent(
+            errorSchema({
+                message: "Invalid Request",
+            }),
+            HttpPhrases.BAD_REQUEST,
+        ),
+        [HttpStatus.UNAUTHORIZED]: jsonContent(
+            errorSchema({
+                message: "Unauthorized",
+            }),
+            HttpPhrases.UNAUTHORIZED,
+        ),
+        [HttpStatus.INTERNAL_SERVER_ERROR]: jsonContent(
+            errorSchema({
+                message: "Internal Server Error",
+            }),
+            HttpPhrases.INTERNAL_SERVER_ERROR,
+        ),
+        [HttpStatus.NOT_FOUND]: jsonContent(
+            errorSchema({
+                message: "Channel not found",
+            }),
+            HttpPhrases.NOT_FOUND,
+        )
+    },
+});
+
 export type CreateChannelRoute = typeof createChannelRoute;
+export type GetWorkspaceChannel = typeof getWorkspaceChannelRoute;
+
 export const channelRoute = createRouter()
-    .openapi( createChannelRoute, createChannelHandler)
-  
+    .openapi(createChannelRoute, createChannelHandler)
+    .openapi(getWorkspaceChannelRoute, getWorkspaceChannelHandler)
 
