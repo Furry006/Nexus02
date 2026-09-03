fea890ac6fe7f3d7766a282100ea16993f09ed24
Mukul  Singh <ms0897493@gmail.com>
2026-09-04 03:08:19 +0530

    feat: implement deleteMessageRoute for message deletion functionality

--- apps/server/src/modules/message/routes.ts
+++ apps/server/src/modules/message/routes.ts
@@ -10,9 +10,9 @@ import {
 import { HttpPhrases, HttpStatus } from "#/utils/http/index.js";
 import { authMiddleware } from "#/middlewares/auth.js";
 
-import { messageTargetParamSchema, createMessageSchema, getMessagesQuerySchema } from "./schemas.js";
+import { messageTargetParamSchema, createMessageSchema, getMessagesQuerySchema, deleteMessageIdParamSchema } from "./schemas.js";
 
-import { createMessageHandler, getMessageHandler } from "./handlers.js";
+import { createMessageHandler, deleteMessageHandler, getMessageHandler } from "./handlers.js";
 
 export const createMessageRoute = createRoute({
   tags: Tags.Message,
@@ -89,12 +89,51 @@ export const getMessageRoute = createRoute({
   },
 });
 
+export const deleteMessageRoute = createRoute({
+  tags: Tags.Message,
+  method: "delete",
+  path: "/{messageId}",
+  middleware: [authMiddleware],
+  
+  request: {
+    params: deleteMessageIdParamSchema
+  },
+  responses: {
+    [HttpStatus.OK]: jsonContent(
+      successSchema({
+        message: "message deleted successfully",
+      }),
+      HttpPhrases.OK
+    ),
+    [HttpStatus.BAD_REQUEST]: jsonContent(
+      errorSchema({
+        message: "invaild Request",
+      }),
+      HttpPhrases.BAD_REQUEST
+    ),
+    [HttpStatus.UNAUTHORIZED]: jsonContent(
+      errorSchema({
+        message: "Unauthorized"
+      }),
+      HttpPhrases.UNAUTHORIZED
+    ),
+    [HttpStatus.NOT_FOUND]: jsonContent(
+      errorSchema({
+        message: "Message Not found"
+      }),
+      HttpPhrases.NOT_FOUND
+    ),
+  }
+})
+
 
 export type CreateMessageRoute = typeof createMessageRoute;
 export type GetMessageRoute = typeof getMessageRoute;
+export type DeleteMessageRoute = typeof deleteMessageRoute;
 
 export const messageRoute = createRouter()
   .openapi( createMessageRoute, createMessageHandler )
-  .openapi( getMessageRoute, getMessageHandler );
+  .openapi( getMessageRoute, getMessageHandler )
+  .openapi(deleteMessageRoute, deleteMessageHandler)
 
 
